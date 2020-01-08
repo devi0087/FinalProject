c9f5eca6bda5a2ab85de79b7780406f53587e55b
komaldevi <devi0087@algonquinlive.com>
2020-01-08 12:13:56 -0500

    finsihed step e

--- RunFinalProject.sh
+++ RunFinalProject.sh
@@ -5,3 +5,4 @@ git init
 cp -r /Library/WebServer/Documents/*htm* ~/FinalProjectRepository
 git add .
 git commit -m "copied html files"
+curl -u "devi0087" https://api.github.com/user/repos -d '{"name":"FinalProjectRepository"}'
