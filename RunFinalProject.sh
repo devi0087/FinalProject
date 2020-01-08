19b286abd95817c38ef7a483f5e88773f0e50bc0
komaldevi <devi0087@algonquinlive.com>
2020-01-08 12:30:27 -0500

    finsihed step f

--- RunFinalProject.sh
+++ RunFinalProject.sh
@@ -6,3 +6,5 @@ cp -r /Library/WebServer/Documents/*htm* ~/FinalProjectRepository
 git add .
 git commit -m "copied html files"
 curl -u "devi0087" https://api.github.com/user/repos -d '{"name":"FinalProjectRepository"}'
+git remote add origin https://github.com/devi0087/FinalProjectRepository.git
+git push -u origin master
