39abf46a5315cb01af189db6362dcc0a8906a1ca
komaldevi <devi0087@algonquinlive.com>
2020-01-12 13:44:49 -0500

    finsihed step g

--- RunFinalProject.sh
+++ RunFinalProject.sh
@@ -8,3 +8,5 @@ git commit -m "copied html files"
 curl -u "devi0087" https://api.github.com/user/repos -d '{"name":"FinalProjectRepository"}'
 git remote add origin https://github.com/devi0087/FinalProjectRepository.git
 git push -u origin master
+
+ssh  devi0087@13.68.133.107 'git clone https://github.com/devi0087/FinalProjectRepository.git;cd FinalProjectRepository; rm -rf .git; cd ..; sudo mv FinalProjectRepository /var/www/html; sudo systemctl start nginx'
