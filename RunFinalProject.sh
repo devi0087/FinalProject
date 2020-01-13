4e26aa28a83dd0932c3535bc4a3db5e2a565a2e3
devi0087 <54950186+devi0087@users.noreply.github.com>
2020-01-12 19:42:16 -0500

    Update RunFinalProject.sh

--- RunFinalProject.sh
+++ RunFinalProject.sh
@@ -12,6 +12,8 @@ git push -u origin master
 ssh  devi0087@13.68.133.107 'git clone https://github.com/devi0087/FinalProjectRepository.git;cd FinalProjectRepository; rm -rf .git; cd ..; sudo mv FinalProjectRepository /var/www/html; sudo systemctl start nginx'
 open -a Safari http://devi0087.hopto.org
 /Users/komaldevi/Library/Android/sdk/emulator/emulator -avd Nexus_10_API_25 &
+/Users/komaldevi/Library/Android/sdk/platform-tools/adb shell am start -a "android.intent.action.VIEW" -d "http://devi0087.hopto.org"
+
 
 
 
